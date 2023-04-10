197fdd98acc75fa4a4a4149d3364b5a42947f1a4
Ermiyas B <124329320+Ermi-B@users.noreply.github.com>
2023-04-10 17:39:09 -0500

    time display format fix and structural adjustment finalized

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,8 @@ var container = $('.container')
 //dayjs
 var currentTime = dayjs().format('h')
 var currentDate = dayjs().format('MM-DD-YYYY')
-var timeInt = parseInt(currentTime) //converts the current time into an integer
+var amPm = dayjs().format('A') //am/pm
+var timeInt = parseInt(currentTime)+12 //converts the current time into an integer and adds 12 to convert to 24 hour format
 
 console.log(timeInt)
 $('#date').text(currentDate) //sets the header with today's date
@@ -15,22 +16,23 @@ $('#date').text(currentDate) //sets the header with today's date
 console.log(currentTime)
 
 //looping through 24 hours of the day
-for (let i = 1; i <= 24; i++) {
+for (let i = 0; i < 24; i++) {
+    var thisHour = dayjs().hour(i) //
    
     var row = $('<div>')    //creating a div with class name row - it contains 3 sections in one row
     row.addClass('row')
 
     var time = $('<h3>')
     time.attr('style','float:right;')
-    time.text(i)
+    time.text(thisHour.format('h A'))
     var div1 = $('<div>') //section 1 to display time
-    div1.addClass('col-2');
+    div1.addClass('col-1');
     div1.append(time)
     div1.attr('style',"border:1px solid black; border-left:none;")
     row.append(div1); //appending section 1 into a row
 
     var div2 = $('<div>') //section 2 to display the input section to type in task name
-    div2.addClass('col-8');
+    div2.addClass('col-10');
     var inputEl = $('<input>')
     if(i==timeInt){
         inputEl.attr('style','height:70px; border: black solid 1px; width:100%; background-color:red;');
@@ -46,11 +48,10 @@ for (let i = 1; i <= 24; i++) {
     row.append(div2);  //appending section 2 into a row
 
     var div3 = $('<div>') //section 3 - save icon
-    div3.addClass('col-2');
+    div3.addClass('col-1');
     
     div3.attr('style',"border:1px solid black; border-right:none;")
-    row.append(div3); //appending section 3 into the row
-   
+    row.append(div3); //appending section 3 into the row  
     
     
     container.append(row); //entire row gets append to the container
