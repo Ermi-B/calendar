ec2928386943e368640e5e0a1096ee62b3c92aa0
Ermiyas B <124329320+Ermi-B@users.noreply.github.com>
2023-04-10 01:11:05 -0500

    javascript generates the calendar list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
-console.log('connected')
+
+var currentDate = dayjs().format('D')
+var numOfCurrentDate = parseInt(currentDate)
+
+for (let i = 1; i <= 30; i++) {
+    var liEl = $('<li>');
+    var inputEl = $('<input>')
+    liEl.attr('style','height:70px; border: black solid 1px;');
+    inputEl.attr('style','height:70px; border: black solid 1px; width:100%;');
+    if(i==numOfCurrentDate){
+        liEl.attr('style','height:70px; border: black solid 1px; background-color:red;');
+    }else if(i<=numOfCurrentDate){
+        liEl.attr('style','height:70px; border: black solid 1px; background-color:gray;');
+    }else{
+        liEl.attr('style','height:70px; border: black solid 1px; background-color:green;');
+    }
+    liEl.append(inputEl);
+    $('.calendar-list').append(liEl); 
+}
