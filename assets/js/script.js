10b9768dea03e35833c50c080c89de429082cd5e
Ermiyas B <124329320+Ermi-B@users.noreply.github.com>
2023-04-10 17:24:56 -0500

    added comments in javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,25 +10,26 @@ var currentDate = dayjs().format('MM-DD-YYYY')
 var timeInt = parseInt(currentTime) //converts the current time into an integer
 
 console.log(timeInt)
-$('#date').text(currentDate)
+$('#date').text(currentDate) //sets the header with today's date
 
 console.log(currentTime)
 
+//looping through 24 hours of the day
 for (let i = 1; i <= 24; i++) {
    
-    var row = $('<div>')    
+    var row = $('<div>')    //creating a div with class name row - it contains 3 sections in one row
     row.addClass('row')
 
     var time = $('<h3>')
     time.attr('style','float:right;')
     time.text(i)
-    var div1 = $('<div>') 
+    var div1 = $('<div>') //section 1 to display time
     div1.addClass('col-2');
     div1.append(time)
     div1.attr('style',"border:1px solid black; border-left:none;")
-    row.append(div1);
+    row.append(div1); //appending section 1 into a row
 
-    var div2 = $('<div>')
+    var div2 = $('<div>') //section 2 to display the input section to type in task name
     div2.addClass('col-8');
     var inputEl = $('<input>')
     if(i==timeInt){
@@ -42,17 +43,17 @@ for (let i = 1; i <= 24; i++) {
 
 
     
-    row.append(div2);
+    row.append(div2);  //appending section 2 into a row
 
-    var div3 = $('<div>') 
+    var div3 = $('<div>') //section 3 - save icon
     div3.addClass('col-2');
     
     div3.attr('style',"border:1px solid black; border-right:none;")
-    row.append(div3);
+    row.append(div3); //appending section 3 into the row
    
     
     
-    container.append(row); 
+    container.append(row); //entire row gets append to the container
     
     
 }
