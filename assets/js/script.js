bc08ef57d9530509192c9a05270bba568e7eb33a
Ermiyas B <124329320+Ermi-B@users.noreply.github.com>
2023-04-10 23:09:51 -0500

    saving task to local storage functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,7 +20,7 @@ for (let i = 0; i < 24; i++) {
     var thisHour = dayjs().hour(i) //
    
     var row = $('<div>')    //creating a div with class name row - it contains 3 sections in one row
-    row.addClass('row')
+    row.addClass('row row-num-' + i)
 
     var time = $('<h3>')
     time.attr('style','float:right;')
@@ -33,30 +33,48 @@ for (let i = 0; i < 24; i++) {
 
     var div2 = $('<div>') //section 2 to display the input section to type in task name
     div2.addClass('col-10');
-    var inputEl = $('<input>')
-
+    var inputEl = $(`<input id=input-element-${i}>`)
+    inputEl.addClass('input-element')
     //color-coding depending on time if its past present or future
     if(i==timeInt){
-        inputEl.attr('style','height:70px; border: black solid 1px; width:100%; background-color:red;');
+        inputEl.attr('style','font-size:150%; height:70px; border: black solid 1px; width:100%; background-color:red;');
     }else if(i<=timeInt){
-        inputEl.attr('style','height:70px; border: black solid 1px; width:100%; background-color:gray;');
+        inputEl.attr('style','font-size:150%; height:70px; border: black solid 1px; width:100%; background-color:gray;');
     }else{
-        inputEl.attr('style','height:70px; border: black solid 1px; width:100%; background-color:green;');
+        inputEl.attr('style','font-size:150%; height:70px; border: black solid 1px; width:100%; background-color:green;');
     } 
+    
     div2.append(inputEl)    
     row.append(div2);  //appending section 2 into a row
 
     var div3 = $('<div>') //section 3 - save icon
     div3.addClass('col-1');
     var saveButton = $('<button>')
-    saveButton.attr('style','height:40px; width:50px; margin-top:10px; background-color:rgb(124, 112, 212); color:white;');
+    saveButton.addClass('save-button')
+    saveButton.attr({
+        'style':'height:40px; width:50px; margin-top:10px; background-color:rgb(124, 112, 212); color:white;',
+        'id':'save-button-'+i,
+    });
     saveButton.text('Save')
+
+  
+      
     div3.append(saveButton)
-    div3.attr('style',"border:1px solid black; border-right:none;")
+    div3.attr('style'," background-color:rgb(237, 102, 240);border:1px solid black;  border-top-right-radius:20px; border-bottom-right-radius:20px;")
     row.append(div3); //appending section 3 into the row  
     
     
     container.append(row); //entire row gets append to the container
+    var localContent = localStorage.getItem(i)
+    var inputContent = $(`#input-element-${i}`).val(localContent)
+    
+    
+      //event listener for the save button
+   $(`#save-button-${i}`).on('click',function(){
+    var inputContent = $(`#input-element-${i}`).val()
+    
+    localStorage.setItem(i,inputContent) //saves the current user input inot local storage when save button clicked
+})
     
     
 }
